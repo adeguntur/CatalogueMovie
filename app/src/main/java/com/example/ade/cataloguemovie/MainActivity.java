38d3351df5c75bc93e1c4d92149bb7a5143bb50c
rehanarroihan <third2014project@gmaill.com>
2018-02-21 19:59:56 +0700

    Get ItemID

--- app/src/main/java/com/example/ade/cataloguemovie/MainActivity.java
+++ app/src/main/java/com/example/ade/cataloguemovie/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,12 +16,26 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<FilmItems>> {
 
+    static final String EXTRAS_FILM = "EXTRAS_FILM";
     ListView listView;
     FilmAdapter adapter;
     EditText editFilm;
     Button buttonCari;
 
-    static final String EXTRAS_FILM = "EXTRAS_FILM";
+    ArrayList<FilmItems> mList = new ArrayList<>();
+    View.OnClickListener myListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String film = editFilm.getText().toString();
+
+            if (TextUtils.isEmpty(film)) return;
+
+            Bundle bundle = new Bundle();
+            bundle.putString(EXTRAS_FILM, film);
+            getLoaderManager().restartLoader(0, bundle, MainActivity.this);
+        }
+    };
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +43,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         setContentView(R.layout.activity_main);
         adapter = new FilmAdapter(this);
         adapter.notifyDataSetChanged();
-        listView = (ListView) findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
 
         listView.setAdapter(adapter);
 
-        editFilm = (EditText) findViewById(R.id.edit_film);
-        buttonCari = (Button) findViewById(R.id.btn_cari);
+        editFilm = findViewById(R.id.edit_film);
+        buttonCari = findViewById(R.id.btn_cari);
 
         buttonCari.setOnClickListener(myListener);
 
@@ -41,6 +57,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         bundle.putString(EXTRAS_FILM, film);
 
         getLoaderManager().initLoader(0, bundle, this);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                Log.d(TAG, "onItemClick: id : " + mList.get(pos).getId());
+            }
+        });
     }
 
     @Override
@@ -55,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onLoadFinished(Loader<ArrayList<FilmItems>> loader, ArrayList<FilmItems> data) {
-
+        mList = data;
         adapter.setData(data);
     }
 
@@ -63,17 +86,4 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<ArrayList<FilmItems>> loader) {
         adapter.setData(null);
     }
-
-    View.OnClickListener myListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String film = editFilm.getText().toString();
-
-            if (TextUtils.isEmpty(film)) return;
-
-            Bundle bundle = new Bundle();
-            bundle.putString(EXTRAS_FILM, film);
-            getLoaderManager().restartLoader(0, bundle, MainActivity.this);
-        }
-    };
 }
